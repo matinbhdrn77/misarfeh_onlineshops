037b7fd12b01c1894021971d3045b5fa88d506b2
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-08-08 11:51:30 +0330

    indexes for product and shop id for faster searching

---
+++ migrations/000011_add_images_indexes.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS images_product_id_idx;
+
+DROP INDEX IF EXISTS images_shop_id_idx;
---
+++ migrations/000011_add_images_indexes.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS images_product_id_idx ON images (product_id);
+CREATE INDEX IF NOT EXISTS images_shop_id_idx ON images (shop_id);
