9141cbcb6674cc08686367c00fc45386d4c62d8e
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-08-08 17:05:25 +0330

    convert it to ip based rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
 
 	"golang.org/x/time/rate"
 )
@@ -33,19 +35,36 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	// Initialize a new rate limiter which allows an average of 2 requests per second,
-	// with a maximum of 4 requests in a single ‘burst’.
-	limiter := rate.NewLimiter(2, 4)
-	// The function we are returning is a closure, which 'closes over' the limiter
-	// variable.
+
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Call limiter.Allow() to see if the request is permitted, and if it's not,
-		// then we call the rateLimitExceededResponse() helper to return a 429 Too Many
-		// Requests response (we will create this helper in a minute).
-		if !limiter.Allow() {
+
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		// Call the Allow() method on the rate limiter for the current IP address. If
+		// the request isn't allowed, unlock the mutex and send a 429 Too Many Requests
+		// response, just like before.
+		if !clients[ip].Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		mu.Unlock()
 		next.ServeHTTP(w, r)
 	})
 }
