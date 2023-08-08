acf8d1e015fbbb60543d9c333d08616b5df8fc70
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-08-08 11:23:34 +0330

    change migrations numbers

--- migrations/000007_create_categories_table.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS categories (
-    id serial PRIMARY KEY,
-    name text NOT NULL UNIQUE,
-    img_url text
-);
--- migrations/000009_add_products_check_constraints.down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE products DROP CONSTRAINT IF EXISTS products_off_check;
-
-ALTER TABLE products DROP CONSTRAINT IF EXISTS products_price_check;
-
-ALTER TABLE products DROP CONSTRAINT IF EXISTS products_sale_price_check;
