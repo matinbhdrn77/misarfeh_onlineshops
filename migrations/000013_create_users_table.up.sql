207cc0ad4108131848fe620bb4321753dab3a24c
matinbhdrn77 <matinbhdrn77@gmail.com>
2023-08-09 14:42:35 +0330

    set phone field required and uniqe for activation

--- migrations/000013_create_users_table.up.sql
+++ migrations/000013_create_users_table.up.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS users (
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     first_name text NOT NULL,
     last_name text NOT NULL,
-    phone text NOT NULL,
-    email citext UNIQUE NOT NULL,
+    phone text UNIQUE NOT NULL,
+    email citext,
     password_hash bytea NOT NULL,
     activated bool NOT NULL,
     version integer NOT NULL DEFAULT 1
